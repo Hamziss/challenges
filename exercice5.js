9a64c28b48b218f55471e35addd412d9b22f5de9
Hamza <96361868+Hamziss@users.noreply.github.com>
2024-01-25 18:29:08 +0100

    Update exercice5.js

--- exercice5.js
+++ exercice5.js
@@ -1,8 +1,3 @@
-//Exercise 5 (10 points)
-
-
-//- Use for loop to iterate from 0 to 100 and print the sum of all numbers.
-
 //- exercice 5 (45 points)
 
 //- Use for loop to iterate from 0 to 100 and print the sum of all evens
